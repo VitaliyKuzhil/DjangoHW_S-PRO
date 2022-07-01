99575e4ab347b74e2a63af374d380216b4f60616
Vitaliy Kuzhil <vitaliy.kuzhil2018@gmail.com>
2022-07-01 17:33:38 +0300

    Home Work W3

---
+++ bookstore/forms.py
@@ -0,0 +1,45 @@
+from django import forms
+from django.utils.translation import gettext_lazy as _
+from bookstore.models import Book, Author
+
+
+class BookForm(forms.ModelForm):
+    book_title = forms.CharField(max_length=50,
+                                 label='Назва книжки',
+                                 widget=forms.TextInput(attrs={'class': 'form-control'})
+                                 )
+    book_description = forms.CharField(label='Опис книжки',
+                                       widget=forms.Textarea(attrs={'class': 'form-control', "rows": 3})
+                                       )
+    released_year = forms.IntegerField(label='Рік видавництва',
+                                       max_value=9999,
+                                       widget=forms.TextInput(attrs={'class': 'form-control'})
+                                       )
+
+    class Meta:
+        model = Book
+        fields = ['book_title', 'book_description', 'released_year', 'author']
+        labels = {
+            'author': _('Автор')
+        }
+        widgets = {
+            'author': forms.Select(attrs={'class': 'form-control'})
+        }
+
+
+class AuthorForm(forms.ModelForm):
+    first_name = forms.CharField(max_length=50,
+                                 label='Імя автора',
+                                 widget=forms.TextInput(attrs={'class': 'form-control'})
+                                 )
+    last_name = forms.CharField(max_length=50,
+                                label='Фамілія автора',
+                                widget=forms.TextInput(attrs={'class': 'form-control'})
+                                )
+    about_author = forms.CharField(label='Біографія',
+                                   widget=forms.Textarea(attrs={'class': 'form-control', "rows": 3})
+                                   )
+
+    class Meta:
+        model = Author
+        fields = ['first_name', 'last_name', 'about_author']
--- bookstore/urls.py
+++ bookstore/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from .views import book_list, book_detail, author_book, books_author
+from .views import book_list, book_detail, author_book, books_author, add_book, author_list, add_author, home
 
 urlpatterns = [
-    path('', book_list, name='book_list'),
+    path('', home, name='home'),
+    path('books/', book_list, name='book_list'),
+    path('authors/', author_list, name='author_list'),
     path('books/<int:book_index>/', book_detail, name='book_detail'),
     path('books/author/<int:author_index>', author_book, name='author_book'),
-    path('books/author/book/<int:author_index>', books_author, name='books_author')
+    path('books/author/book/<int:author_index>', books_author, name='books_author'),
+    path('add_book/', add_book, name="add_book"),
+    path('add_author/', add_author, name="add_author")
 ]
